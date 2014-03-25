31bcf6a635a858a8be0404dfd588515b01dc3762
Chad Meece <mplx@ymail.com>
2014-03-25 15:45:31 -0400

    Solved draw.

--- fifteen/fifteen.c
+++ fifteen/fifteen.c
@@ -31,6 +31,9 @@
 // board, whereby board[i][j] represents row i and column j
 int board[MAX][MAX];
 
+// Char __ for use as blank space
+char SP;
+
 // board's dimension
 int d;
 
@@ -38,7 +41,7 @@ int d;
 void clear(void);
 void greet(void);
 void init(void);
-void swap(int *a, int *b)
+void swap(int *a, int *b);
 void draw(void);
 bool move(int tile);
 bool won(void);
@@ -133,13 +136,29 @@ void greet(void)
 void init(void)
 {
     // Get board dimensions and populate grid with correct number of tiles plus 1 blank, swap '1' and '2' if tile count is odd.
-    for (int i = 0; i <= d; i++)
+    int tile = (d * d); //population of tiles
+    int place = 1; //location helper, order starts at array 0 x 0.
+    
+    for (int i = 0; i <= (d - 1); i++) //iterate through rows.
     {
-        for (int j = 0; j <= d; j++)
+        for (int j = 0; j <= (d - 1); j++) //iterate through columns for each row.
         {
-            board[i][j] = (d * d) - j;
-            printf("board %i x %i = %i\n", i, j, d-j);
+            if ((tile - place) == 0) //test if at the 0th place in population.
+            {
+                board[i][j] = 95; //set 'blank' space to ascii '__'.
+            }
+            else
+            {
+                board[i][j] = tile - place; //populate tile with declining tile set.
+                place++;  //increment population.
+            }
         }
+    }
+    
+    int swaptest = (tile - 1) % 2; // variable equal to remainder of 'tile count sans the underscore tile' / 2.
+    if (swaptest > 0) //test, true if tile count is odd. 
+    {
+        swap(&board[d-1][d-2], &board[d-1][d-3]);
     }
 }
 
@@ -159,7 +178,21 @@ void swap(int* a, int* b)
  */
 void draw(void)
 {
-    // TODO
+    for(int i = 0; i <= (d - 1); i++) //iterate through rows.
+    {
+        for(int j = 0; j <= (d - 1); j++) //iterate through columns for each row.
+        {
+            if(board[i][j] == 95)
+            {
+                printf("%2c  ", 95);
+            }
+            else
+            {
+                printf("%2d  ", board[i][j]); //print current place.
+            }
+        }
+        printf("\n"); //print new line.
+    }
 }
 
 /**
